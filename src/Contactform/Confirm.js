eab05f42be5967c7b070dea4ae26e9dcd8250b26
ryandude97 <94033747+ryandude97@users.noreply.github.com>
2022-08-23 23:33:02 -0400

    process form change

--- src/Contactform/Confirm.js
+++ src/Contactform/Confirm.js
@@ -5,7 +5,7 @@ import Button from "@material-ui/core/Button";
 export default class Confirm extends Component {
   continue = e => {
     e.preventDefault();
-    // PROCESS FORM //
+    // PROCES FORM //
     this.props.nextStep();
   };
 
